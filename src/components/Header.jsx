8cdc5e4e8a1d0a8c16df2aaccd84552f29b918c2
Sem4eZ <semen.fedorov3005@yandex.ru>
2024-03-11 21:28:22 +0500

    добавил отображение количества товаров в корзине

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const cartItems = useSelector((state) => state.cart.items);
   return (
     <header>
       <div
@@ -9,7 +11,12 @@ const Header = () => {
     max-h-[90px] items-center justify-center p-6 px-6 gap-x-6 bg-indigo-200 shadow-xl text-black text-xl"
       >
         <Link to={"/"}>Товары</Link>
-        <Link to={"/cart"}>Корзина</Link>
+        <Link to={"/cart"}>
+          Корзина{" "}
+          <span className="absolute px-2 bg-red-300 rounded-full text-xs">
+            {cartItems.length}
+          </span>
+        </Link>
       </div>
     </header>
   );
